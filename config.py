6b52c8eee8dd098608cda0af9892eccfba066c1d
infoSBCC <business101.0001@gmail.com>
2026-03-20 14:59:06 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# ===== config.py =====
+# เก็บค่าตั้งต้นทั้งหมด แก้ที่นี่ที่เดียว
+
+# Google Sheet ID
+SHEET_ID = "1jOsRV1Q8BbLM1p1HJAdvJ6liK9VlG3sUX1LalDcxSks"
+
+# ชื่อ sheet ที่ใช้งาน
+SHEET_NAME = "test"
+
+# Scope สำหรับ Google Sheets API (อ่าน+เขียน)
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
